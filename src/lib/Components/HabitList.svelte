6a50fed25cda046050311cf1ad380a8c59dc8e97
David <davidcredo00@gmail.com>
2022-03-02 12:31:55 +0100

    added reactivity to habit list

--- src/lib/Components/HabitList.svelte
+++ src/lib/Components/HabitList.svelte
@@ -1,15 +1,23 @@
 <script>
     import ListItem from './ListItem.svelte';
- 
+    import { habits } from '$lib/stores/UserData';
+
+    let habitName = '';
+
+    function addHabit() {
+        $habits = [...$habits, {
+            id: $habits.length, 
+            name: habitName, 
+            completed:false} 
+        ]};
 </script>
 
+{#each $habits as habit}
+    <ListItem title={habit.name}/>
+{/each}
 
-<ListItem></ListItem>  
 
-
-<form action="GET">
 <label for="new_Habit"> 
-<input value="" type="text" id="new_Habit" name="new_Habit" placeholder="new habit">
+<input bind:value={habitName} type="text" id="new_Habit" name="new_Habit" placeholder="new habit">
 </label>
-<button type="submit">Add</button>
-</form>
+<button type="submit" on:click={addHabit}>Add</button>
