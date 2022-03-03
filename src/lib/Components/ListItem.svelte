5d6edf1df65ce77c5a74af0bc90186fbc6bb82d5
David <davidcredo00@gmail.com>
2022-03-03 18:13:22 +0100

    state management of tasks initial integration

--- src/lib/Components/ListItem.svelte
+++ src/lib/Components/ListItem.svelte
@@ -1,27 +1,38 @@
 <script>
-    export let title = "some task";
+	import { habits } from '$lib/stores/UserData';
+
+	export let title = 'some habit';
+
+	const completeHabit = () => {
+		$habits = $habits.completed = true;
+	};
 </script>
 
 <article>
-    <span>{title}</span>
-    <label for="task_done">
-        <input type="checkbox" id="task_done" name="task_done">
-    </label>
+	<span>{title}</span>
+	<label for="task_done">
+		<input
+			type="checkbox"
+			id="task_done"
+			name="task_done"
+			on:click={completeHabit}
+		/>
+	</label>
 </article>
 
 <style>
-    article {
-        display: flex;
-        justify-content: space-between;
-    }
+	article {
+		display: flex;
+		justify-content: space-between;
+	}
 
-    span {
-        padding: 0 10px;
-        font-weight: 600;
-    }
+	span {
+		padding: 0 10px;
+		font-weight: 600;
+	}
 
-    #task_done {
-        width: 2em;
-        height: 2em;
-    }
+	#task_done {
+		width: 2em;
+		height: 2em;
+	}
 </style>
