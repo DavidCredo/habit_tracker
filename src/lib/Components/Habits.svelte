489f7fb45e02681f78d6c1aebda2e81728664029
David <davidcredo00@gmail.com>
2022-03-07 12:09:29 +0100

    added luxon dependency

--- src/lib/Components/Habits.svelte
+++ src/lib/Components/Habits.svelte
@@ -1,7 +1,12 @@
 <script>
-
+    let habits = [
+        {id: 1, name: 'Cold shower', completed: false},
+        {id: 2, name: 'Workout', completed: false}
+    ]
 </script>
 
 <div class="container">
-    
+    <ul>
+        
+    </ul>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,12 @@
 <script>
-import Habits from "$lib/Components/Habits.svelte";
-
+	import Habits from '$lib/Components/Habits.svelte';
 	let name = 'David';
 	let currentPhase = 'Phase 1';
 </script>
 
 <main class="container">
 	<h1>Welcome, {name}.</h1>
-	<h2>{currentPhase}</h2>	
+	<h2>{currentPhase}</h2>
 	<Habits />
 </main>
 
